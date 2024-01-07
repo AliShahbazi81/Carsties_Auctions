68e8d1bb4fd11d991cfccb7c9f627ab352e98ca8
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-06 22:48:30 -0500

    Auction controller

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,8 @@
 using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Controllers;
 
@@ -8,9 +11,34 @@ namespace AuctionService.Controllers;
 public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
 
-    public AuctionsController(AuctionDbContext context)
+    public AuctionsController(
+        AuctionDbContext context,
+        IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAll()
+    {
+        var auctions = await _context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<AuctionDto>> GetById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        return _mapper.Map<AuctionDto>(auction);
     }
 }
