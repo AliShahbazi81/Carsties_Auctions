8a3d8a05a2a4d19d2af4feebc3c0b3c461c67e06
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-10 12:42:13 -0500

    Get access token from postman using Duende

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -14,39 +14,22 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
+            new("auctionApp", "Auction app fill access"),
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
-            new Client
+            new()
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                // Using postman, we do not have any redirect link, but it has to be filled. The url down below is just an example
+                RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
+                ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
+                // Authentication flow
+                AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            }
         };
 }
