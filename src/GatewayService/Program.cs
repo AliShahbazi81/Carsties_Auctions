dfc9b11c969cd6e75a19a31f2024f0832aed045f
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-11 14:26:33 -0500

    GatewayService init files and configs

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Gateway configurations
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
