b11197635160871e5e124740a0cb672898449e59
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-11 14:54:21 -0500

    Add authentication layer to gateway service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Gateway configurations
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+// Add Authentication configuration. This has to be identical with the one which is in the Program.cs inside the auctionService.
+// We have to duplicate the configurations, but this is the way to do it.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // Tells the server who the token was issued by
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        // Since our server is running on HTTP, this has to be false, otherwise -> true
+        options.RequireHttpsMetadata = false;
+        // We have to disable this because we are using the same token for all services
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
