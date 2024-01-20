eb4344dd3cebd7423569ee47c7efcbb60dd60a9e
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-20 13:08:41 -0500

    Add bidding service, rabbitMq, and mongoDb Configurations

--- client/web-app/app/nav/Search.tsx
+++ client/web-app/app/nav/Search.tsx
@@ -17,6 +17,7 @@ export default function Search() {
 	  }
 
 	  function search() {
+			// When the user is 
 			if (pathname !== "/") router.push('/')
 			setParams({searchTerm: searchValue})
 	  }
---
+++ src/BiddingService/Program.cs
@@ -0,0 +1,50 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// MasTransit Rabbit Mq Configurations
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+// Configuration for JWT token
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("BidDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
+
+app.Run();
