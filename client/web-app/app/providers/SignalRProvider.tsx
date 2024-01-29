a579ef57acdcc46f969b99fa6e51d68d1bb58316
Ali Shahbazi <ali.vortex799@gmail.com>
2024-01-29 17:54:40 -0500

    Add AuctionCreated live creation for logged-in users

--- client/web-app/app/providers/SignalRProvider.tsx
+++ client/web-app/app/providers/SignalRProvider.tsx
@@ -3,14 +3,18 @@ import {ReactNode, useEffect, useState} from "react";
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {useAuctionStore} from "@/hooks/useAuctionStore";
 import {useBidStore} from "@/hooks/useBidStore";
-import {Bid} from "@/types";
+import {Auction, Bid} from "@/types";
+import {User} from "next-auth";
+import toast from "react-hot-toast";
+import AuctionCreatedToast from "@/app/components/AuctionCreatedToast";
 
 
 type Props = {
 	  children: ReactNode
+	  user: User | null
 }
 
-export default function SignalRProvider({children}: Props)
+export default function SignalRProvider({children, user}: Props)
 {
 	  // Store the connection - webSocket to backend signalR
 	  const [connection, setConnection] = useState<HubConnection | null>(null)
@@ -44,6 +48,15 @@ export default function SignalRProvider({children}: Props)
 									// For real time showing the bids in detailed page of the cars
 									addBid(bid);
 							  });
+							  
+							  connection.on('AuctionCreated', (auction: Auction) => {
+									if (user?.username !== auction.seller)
+									{
+										  return toast(<AuctionCreatedToast auction={auction} />, 
+												{duration: 10000})
+									}
+							  })
+							  
 						}).catch(error => console.log(error))
 				  
 				  // If we want to stop the connection, we have to return, so that if the user closes the browser or changes the window, the connection will stop
@@ -53,7 +66,7 @@ export default function SignalRProvider({children}: Props)
 						)
 				  }
 			}
-	  }, [connection, setCurrentPrice])
+	  }, [connection, setCurrentPrice, addBid, ])
 	  
 	  return(
 			children
